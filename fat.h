95103348653ab83d72784431da155abf18b3401a
Adam <adamkuchar10@gmail.com>
2026-03-25 07:56:21 +0100

    fixed

--- fat.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef FAT_H
-#define FAT_H
-
-#include "fat_types.h"
-#include "fat_api.h"
-
-#endif
--- fat_linux_adapter.c
+++
@@ -1,7 +0,0 @@
-#include "fat.h"
-
-/*
- * Linux adaptér bol rozdelený do:
- * - fat_linux_adapter_io.c
- * - fat_linux_main.c
- */
